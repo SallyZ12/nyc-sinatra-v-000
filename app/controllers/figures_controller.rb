606c53add90f5c0944b49224f30a1e2a66359fe8
Sally Zasloff <szasloff@gmail.com>
2018-03-06 23:24:40 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,6 +6,23 @@ class FiguresController < ApplicationController
     erb :'/figures/index'
   end
 
+  get '/figures/new' do
+
+      erb :'figures/new'
+  end
+
+
+  post '/figures' do
+    @figure = Figure.create(name: params[:figure][:name])
+  
+    redirect "/figures/#{@figure.name}"
+  end
+
+  get '/figures/:id' do
+    @figures = Figure.find_by_id(params[:id])
+
+    erb :'figures/show'
+  end
 
 
 end
